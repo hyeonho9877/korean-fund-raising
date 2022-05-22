bce0bf9833b8bb807bbf0f27a50573543fa804ca
qwerqwer0221 <84187227+qwerqwer0221@users.noreply.github.com>
2022-05-23 00:03:44 +0900

    small update

--- index/index.js
+++ index/index.js
@@ -6,14 +6,14 @@ async function connect() {
         console.log(error);
       }
       const accounts = await ethereum.request({ method: "eth_accounts" });
-      document.getElementById("btnConnect").innerHTML = "Connected";
+      document.getElementById("btnConnect").innerHTML = "연결됨";
 
     //   dash board 보이기
       document.getElementById("dashBoard").style.display = "block";
 
     } else {
       document.getElementById("btnConnect").innerHTML =
-        "Please install MetaMask";
+        "MetaMask를 설치해주세요";
     }
   }
   
