ae7acda9c5ab919217a12d920fea09b5e3259030
Ch1keen <gihoong7@gmail.com>
2022-05-22 17:35:06 +0900

    fix: Typo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // "GET / "을 수행할 때 index 폴더에 있는 .html과 .js 파일을 렌더링합니다
 app.use(express.static('index'))
-app.use('detail', express.static('detail'))
+app.use('/detail', express.static('detail'))
 
 app.listen(port, () => {
   console.log(`Korean Fund Raising app is running in port ${port}`)
