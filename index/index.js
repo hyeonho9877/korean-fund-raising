c65c434dfd861790fb811df2ebd8910dd709bbef
qwerqwer0221 <84187227+qwerqwer0221@users.noreply.github.com>
2022-05-30 09:24:14 +0900

    web3

--- index/index.js
+++ index/index.js
@@ -4,25 +4,20 @@ function load_first() {
 }
 
 async function initial() {
-  var Web3 = require('web3');
-  var web3 = new Web3("http://localhost:7545");
-  // web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-}
+  // var Web3 = require('web3');
+  var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
-
-// var managerContract = web3.eth.Contract([{ "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "contractAddress", "type": "address" }, { "indexed": false, "internalType": "address", "name": "projectStarter", "type": "address" }, { "indexed": false, "internalType": "string", "name": "projectTitle", "type": "string" }, { "indexed": false, "internalType": "string", "name": "projectDesc", "type": "string" }], "name": "groupCreated", "type": "event" }, { "inputs": [{ "internalType": "string", "name": "title", "type": "string" }, { "internalType": "string", "name": "description", "type": "string" }], "name": "createGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getMyGroup", "outputs": [{ "internalType": "address[10]", "name": "", "type": "address[10]" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }], "name": "getMyGroupCount", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }, { "internalType": "address", "name": "groupAddress", "type": "address" }, { "internalType": "uint8", "name": "seat", "type": "uint8" }], "name": "updateMyGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }])
-// .at("0x97FDe74B9d75980112c23710eA10b44432E05FaA");
-
-async function a() {
-  var Contract = require('web3-eth-contract');
   const contractAddress = "0x97FDe74B9d75980112c23710eA10b44432E05FaA";
   const contractABI = [{ "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "contractAddress", "type": "address" }, { "indexed": false, "internalType": "address", "name": "projectStarter", "type": "address" }, { "indexed": false, "internalType": "string", "name": "projectTitle", "type": "string" }, { "indexed": false, "internalType": "string", "name": "projectDesc", "type": "string" }], "name": "groupCreated", "type": "event" }, { "inputs": [{ "internalType": "string", "name": "title", "type": "string" }, { "internalType": "string", "name": "description", "type": "string" }], "name": "createGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getMyGroup", "outputs": [{ "internalType": "address[10]", "name": "", "type": "address[10]" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }], "name": "getMyGroupCount", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }, { "internalType": "address", "name": "groupAddress", "type": "address" }, { "internalType": "uint8", "name": "seat", "type": "uint8" }], "name": "updateMyGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
-  var managerContract = new Contract(contractABI, contractAddress);
+  var managerContract = new web3.eth.Contract(contractABI, contractAddress);
   managerContract.methods.createGroup('a', 'b').call();
-
+  console.log("account", web3.eth.getAccounts());
 }
 
-
+async function manager() {
+  window.web
+  // managerContract.methods.createGroup('a', 'b').call();
+}
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
@@ -42,22 +37,3 @@ async function connect() {
       "MetaMask를 설치해주세요";
   }
 }
-
-// async function a() {
-
-//   const title = 'a';
-//   const description = 'b';
-
-//   const contractAddress = "0x97FDe74B9d75980112c23710eA10b44432E05FaA";
-//   const contractABI = [{ "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "contractAddress", "type": "address" }, { "indexed": false, "internalType": "address", "name": "projectStarter", "type": "address" }, { "indexed": false, "internalType": "string", "name": "projectTitle", "type": "string" }, { "indexed": false, "internalType": "string", "name": "projectDesc", "type": "string" }], "name": "groupCreated", "type": "event" }, { "inputs": [{ "internalType": "string", "name": "title", "type": "string" }, { "internalType": "string", "name": "description", "type": "string" }], "name": "createGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getMyGroup", "outputs": [{ "internalType": "address[10]", "name": "", "type": "address[10]" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }], "name": "getMyGroupCount", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }, { "internalType": "address", "name": "groupAddress", "type": "address" }, { "internalType": "uint8", "name": "seat", "type": "uint8" }], "name": "updateMyGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
-//   const provider = new ethers.providers.Web3Provider(window.ethereum);
-//   const signer = provider.getSigner();
-//   const contract = new ethers.Contract(contractAddress, abi, signer);
-
-//   managerContract = await new window.web3.eth.Contract(contractABI, contractAddress);
-
-// }
-
-// async function a(){
-//   managerContract.createGroup('a','b');
-// }
