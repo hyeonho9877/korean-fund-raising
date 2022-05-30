ddc2946bc57f7e782b5293e9a9d2c09913cdf090
raradaf <104483730+raradaf@users.noreply.github.com>
2022-05-30 16:41:24 +0900

    abi looks better

--- index/index.js
+++ index/index.js
@@ -1,191 +1,6 @@
 const contractAddress = "0x425180500861c9f117d493aFD237488aEaa65fca";
 const contractABI = [{ "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "contractAddress", "type": "address" }, { "indexed": false, "internalType": "address", "name": "projectStarter", "type": "address" }, { "indexed": false, "internalType": "string", "name": "projectTitle", "type": "string" }, { "indexed": false, "internalType": "string", "name": "projectDesc", "type": "string" }], "name": "groupCreated", "type": "event" }, { "inputs": [{ "internalType": "string", "name": "title", "type": "string" }, { "internalType": "string", "name": "description", "type": "string" }], "name": "createGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getMyGroup", "outputs": [{ "internalType": "address[10]", "name": "", "type": "address[10]" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }], "name": "getMyGroupCount", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function", "constant": true }, { "inputs": [{ "internalType": "address", "name": "target", "type": "address" }, { "internalType": "address", "name": "groupAddress", "type": "address" }, { "internalType": "uint8", "name": "seat", "type": "uint8" }], "name": "updateMyGroup", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
-const groupABI = [
-  {
-    "inputs": [
-      {
-        "internalType": "address payable",
-        "name": "owner",
-        "type": "address"
-      },
-      {
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "desc",
-        "type": "string"
-      },
-      {
-        "internalType": "address",
-        "name": "_GroupManager",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "groupAddress",
-        "type": "address"
-      },
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
-      }
-    ],
-    "name": "alreadyJoined",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "uint32",
-        "name": "groupID",
-        "type": "uint32"
-      },
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
-      }
-    ],
-    "name": "notMember",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [],
-    "name": "outOfLimit",
-    "type": "event"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
-      }
-    ],
-    "name": "transferAdmit",
-    "type": "event"
-  },
-  {
-    "inputs": [],
-    "name": "deposit",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "requestWithdraw",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "agree",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "cancelRequest",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getDetails",
-    "outputs": [
-      {
-        "internalType": "address payable",
-        "name": "owner",
-        "type": "address"
-      },
-      {
-        "internalType": "string",
-        "name": "name",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "desc",
-        "type": "string"
-      },
-      {
-        "internalType": "uint256",
-        "name": "balance",
-        "type": "uint256"
-      },
-      {
-        "internalType": "address[10]",
-        "name": "membersResponse",
-        "type": "address[10]"
-      },
-      {
-        "internalType": "address",
-        "name": "currentRequester",
-        "type": "address"
-      },
-      {
-        "internalType": "bool",
-        "name": "isCurrentRaising",
-        "type": "bool"
-      },
-      {
-        "internalType": "uint256",
-        "name": "currentRequestAmount",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "joinGroup",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "result",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
+const groupABI = [{"inputs": [{"internalType": "address payable","name": "owner","type": "address"},{"internalType": "string","name": "name","type": "string"},{"internalType": "string","name": "desc","type": "string"},{"internalType": "address","name": "_GroupManager","type": "address"}],"stateMutability": "nonpayable","type": "constructor"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "address","name": "groupAddress","type": "address"},{"indexed": false,"internalType": "address","name": "from","type": "address"}],"name": "alreadyJoined","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "uint32","name": "groupID","type": "uint32"},{"indexed": false,"internalType": "address","name": "from","type": "address"}],"name": "notMember","type": "event"},{"anonymous": false,"inputs": [],"name": "outOfLimit","type": "event"},{"anonymous": false,"inputs": [{"indexed": false,"internalType": "uint256","name": "amount","type": "uint256"},{"indexed": false,"internalType": "address","name": "to","type": "address"}],"name": "transferAdmit","type": "event"},{"inputs": [],"name": "deposit","outputs": [],"stateMutability": "payable","type": "function"},{"inputs": [{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "requestWithdraw","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "agree","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "cancelRequest","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "getDetails","outputs": [{"internalType": "address payable","name": "owner","type": "address"},{"internalType": "string","name": "name","type": "string"},{"internalType": "string","name": "desc","type": "string"},{"internalType": "uint256","name": "balance","type": "uint256"},{"internalType": "address[10]","name": "membersResponse","type": "address[10]"},{"internalType": "address","name": "currentRequester","type": "address"},{"internalType": "bool","name": "isCurrentRaising","type": "bool"},{"internalType": "uint256","name": "currentRequestAmount","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "joinGroup","outputs": [{"internalType": "bool","name": "result","type": "bool"}],"stateMutability": "nonpayable","type": "function"}];
 var web3;
 
 window.onload = load_first;
@@ -199,8 +14,8 @@ async function load_first() {
   // var tx = await managerContract.methods.createGroup('a', 'b').send({from: addresses[0], gas: createGroupGas});
 
   // console.log(tx.transactionHash);
-  
-  
+
+
   managerContract.methods.getMyGroup().call(async (err, res) => {
     document.getElementById('dashBoardBody').innerHTML = await Promise.all(
       res.map(async (e) => {
@@ -213,15 +28,15 @@ async function load_first() {
 
           var balance = await web3.eth.getBalance(e);
 
-          return "<tr>" + 
-            "<th scope=\"row\">" + e + "</th>"+   // 이름
-            "<td>Ipsum</td>"+                 // 기간
-            "<td>" + balance +"</td>" +                 // 금액
-            "<td><div id=\"btnDetail\" onclick=\"location.href='../detail/index.html'\" style=\"cursor: pointer;\">그룹 세부정보</div></td>"+
-          "</tr>";
+          return "<tr>" +
+            "<th scope=\"row\">" + e + "</th>" +   // 이름
+            "<td>Ipsum</td>" +                 // 기간
+            "<td>" + balance + "</td>" +                 // 금액
+            "<td><div id=\"btnDetail\" onclick=\"location.href='../detail/index.html'\" style=\"cursor: pointer;\">그룹 세부정보</div></td>" +
+            "</tr>";
         }
       }
-    ));
+      ));
   })
 }
 
